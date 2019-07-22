44d78f3757c2cacb657e3489727eee96ba733b50
Emmanuel <manu8_93@msn.com>
2019-07-22 15:19:54 +0200

    route

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,10 +2,13 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController
 {
-
+    /**
+     * @Route("/", name="number")
+     */
    public function number($max = 9)
    {
        $number = random_int(0, $max);
@@ -15,13 +18,18 @@ class DefaultController
            '<html><body>Lucky number: '.$number.'</body></html>'
        );
    }
-
+    /**
+     * @Route("/dynamic/{max}/{min}", name="dynamic")
+     */
    public function dynamic($max, $min) {
     $number = random_int($min, $max);
 
        return new Response("J'ai compris " . $number);
    }
 
+    /**
+     * @Route("/dynamic_2", name="dynamic_2")
+     */
    public function show_dynamic() {
        return new Response("abc");
    }
